16e353b86798a350e1f0c7ba010b86db4c740d85
home-chengxiaoyu <iwof920125@hotmail.com>
2018-06-10 07:54:34 +0800

    增加说明，并发布

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -11,6 +11,9 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <p className="App-intro">尝试用react开发一个静态Blog</p>
+        <p className="App-intro">尝试使用typescript开发react项目</p>
+        <p className="App-intro">—— by 2018-6-10</p>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
