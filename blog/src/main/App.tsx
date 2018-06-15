a86784b8de0c6b9d86ba2dfefdf371802e96031e
home-chengxiaoyu <iwof920125@hotmail.com>
2018-06-15 21:48:58 +0800

    调整主页样式

--- blog/src/main/App.tsx
+++ blog/src/main/App.tsx
@@ -15,12 +15,15 @@ class App extends React.Component {
           <h1 className="App-title">路过历史的长河，拾取祖先的遗训</h1>
           <h1 className="App-title">——路拾遗</h1>
         </header>
-        <p className="App-intro">尝试用react开发一个静态Blog</p>
-        <p className="App-intro">尝试使用typescript开发react项目</p>
-        <p className="App-intro">——最新更新日期</p>
-        <p className="App-intro">2018年6月14日</p>
+        <body className="App-body">
+          <div className="App-intro">尝试用react开发一个静态Blog</div>
+          <div className="App-intro">尝试使用typescript开发react项目</div>
+          <div className="App-intro">——最新更新日期</div>
+          <div className="App-intro">2018年6月15日</div>
+        </body>
         <footer className="App-footer">
-          <p>湘ICP备15015794号</p>
+          <div>湘ICP备15015794号</div>
+          <div>Copyright © 2018 lushiyi.cn All Rights Reserved</div>
         </footer>
       </div>
     );
